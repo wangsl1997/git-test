5311397f55438128fab49a194349942d168ac373
wsl <wsl@126.com>
2022-08-26 13:31:09 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author wsl
+ * @create 2022-08-26 13:25
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
