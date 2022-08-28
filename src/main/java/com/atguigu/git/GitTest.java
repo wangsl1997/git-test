fe10961018da8b1f9825167f63d7a5a8477e3641
wsl <wsl@126.com>
2022-08-28 09:58:05 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,7 +8,9 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hi git2");
+        System.out.println("hi hot-fix-111");
         System.out.println("hi hot-fix-git");
+        System.out.println("hi hot-fix-121");
 
     }
 }
